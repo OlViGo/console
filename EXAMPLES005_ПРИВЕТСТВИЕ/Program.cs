f3db4b9b64b3dcf21ffcd7c854bed874c1d2e50e
OlViGo <olgabibendum@gmail.com>
2022-10-03 09:37:28 +0300

    Приветстчие

---
+++ EXAMPLES005_ПРИВЕТСТВИЕ/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username == "Masha")
+{
+    Console.WriteLine("Ура, это же Маша ");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
